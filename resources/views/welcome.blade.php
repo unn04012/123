ce280f5c0bee23dcac0ab9fb76a1f5171709247a
unn04012 <unn04012@naver.com>
2020-03-09 08:32:40 +0900

    Laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.master')
+
+@section('style')
+<style media="screen">
+  body {background : green; color : white;}
+</style>
+@endsection
+
+@section('content')
+  @include('partials.footer')
+@endsection
+
+@section('script')
+<script type="text/javascript">
+  alert('저는 자식 뷰의 "script" 섹션입니다');
+</script>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,67 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// with()메서드를 이용
+// Route::get('/', function(){
+//   return view('welcome')->with([
+//     'name' => 'Foo',
+//     'greeting' => '안녕하세요',
+//   ]);
+// });
+
+Route::resource('articles','ArticlesController');
+Route::get('/','WelcomeController@index');
+
+Route::get('auth/login',function(){
+  $credentials = [
+    'email' => 'john@example.com',
+    'password' => 'password'
+  ];
+
+  if(!auth()->attempt($credentials)){
+    return '로그인 정보가 정확하지 않습니다';
+  }
+  return redirect('protected');
+});
+
+Route::get('protected',['middleware' => 'auth', function(){
+  dump(session()->all());
+  return '어서오세요'. auth()->user()->name;
+}]);
+
+Route::get('auth/logout',function(){
+  auth()->logout();
+  return '또 봐요~';
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
