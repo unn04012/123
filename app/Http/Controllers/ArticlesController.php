78036345f2fb72bc26697b93cab7c12ae31f3126
unn04012 <unn04012@naver.com>
2020-03-11 09:26:16 +0900

    입력값 유효성 검사

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -16,25 +16,35 @@ class ArticlesController extends Controller
       return view('articles.create');
     }
 
-    // public function store(Request $request){
-    //   $rules = [
-    //     'title' => ['required'],
-    //     'content' => ['required', 'min:10'],
-    //   ];
-    //
-    //   $validator = \Validator::make($request->all(), $rules);
-    //
-    //   if($validator->fails()){
-    //     return back()->withErrors($validator)
-    //                  -> withInput();
-    //   }
-    //
-    //   $article = \App\User::find(1)->articles()
-    //                                -> create($request->all());
-    //
-    //   if(! $article){
-    //     return back()->with('flash_message', '글이 저장되지 않았습니다.')->withInput();
-    //   }
-    //   return redirect(route('articles.index'))->with('flash_meggate','작성하신 글이 저장되었습니다.');
-    // }
+    public function store(\App\Http\Requests\ArticlesRequest $request){
+      $article = \App\User::find(1)->articles()->create($request->all());
+
+      if(! $article){
+        return back()->with('flash_message', '글이 저장되지 않았습니다.')->withInput();
+      }
+
+      return redirect(route('articles.index'))->with('flash_message','작성하신 글이 저장되었습니다.');
+
+      // $rules = [
+      //   'title' => ['required'],
+      //   'content' => ['required', 'min:10'],
+      // ];
+      //
+      // $message = [
+      //     'title.required' => '제목은 필수 입력 항목입니다',
+      //     'content.required' => '본문은 필수 입력 항목입니다',
+      //     'content.min' => '본문은 최소 :min 글자 이상이 필요합니다',
+      // ];
+      // $validator = \Validator::make($request->all(), $rules, $message);
+      //
+      // if($validator->fails()){
+      //   return back()->withErrors($validator)
+      //                -> withInput();
+      // }
+      //
+      // $article = \App\User::find(1)->articles()
+      //                              -> create($request->all());
+      //
+      //
+    }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -78,7 +78,11 @@
                 </div>
             </div>
         </nav>
-
+        @if(session()->has('flash_message'))
+        <div class="alert alert-info" role = "alert">
+          {{session('flash_message')}}
+        </div>
+        @endif
         <main class="py-4">
             @yield('content')
         </main>
